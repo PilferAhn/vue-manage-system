53c8c8adf5073b5e2ba2a7ae440e7a4f588d05a5
PilferAhn <49383308+PilferAhn@users.noreply.github.com>
2025-01-13 11:45:02 +0900

    Update httpProtocol.ts

--- src/utils/httpProtocol.ts
+++ src/utils/httpProtocol.ts
@@ -1,14 +1,23 @@
 import axios from "axios";
 
 export const sendGetRequest = async (baseUrl: string, uuid: string) => {
-    const url = `${baseUrl}/${uuid}`;
-    
-    try {
-      const response = await axios.get(url);
-      console.log(response.data);
-      return response.data;  // 응답 데이터를 반환
-    } catch (error) {
-      console.error('HTTP GET request failed:', error);
-      throw error;  // 에러를 다시 throw하여 호출자에게 에러를 알림
-    }
-  };
+  const url = `${baseUrl}/${uuid}`;
+
+  try {
+    const response = await axios.get(url);
+    return response.data; // 응답 데이터를 반환
+  } catch (error) {
+    console.error("HTTP GET request failed:", error);
+    throw error; // 에러를 다시 throw하여 호출자에게 에러를 알림
+  }
+};
+
+export const sendPostRequest = async (url: string, formData: FormData) => {
+  try {
+    const response = await axios.post(url, formData);
+    return response.data
+  } catch (error) {
+    console.error("HTTP GET request failed:", error);
+    throw error; // 에러를 다시 throw하여 호출자에게 에러를 알림
+  }
+};
