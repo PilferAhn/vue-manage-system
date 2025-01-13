3361ff483817c471526a169bd4810a9aeb46917a
PilferAhn <49383308+PilferAhn@users.noreply.github.com>
2025-01-13 14:08:29 +0900

    Add User Interface

---
+++ src/interface/user.ts
@@ -0,0 +1,11 @@
+export interface User {
+  id: string;
+  email: string;
+  userName: string;
+  role?: string;
+  msKeys?: string;
+  department?: string;
+  imageName?: string;
+  lastAccess?: Date;
+  lastPasswordChange?: Date;
+}
