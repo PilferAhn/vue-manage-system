debccb9deb08e9c8e2dbe4c332398947d745efab
PilferAhn <49383308+PilferAhn@users.noreply.github.com>
2025-01-20 15:38:44 +0900

    재고 리스트 업데이트 완료

---
+++ src/views/Solder/Stock/Package/Package.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="container">
+    <el-tabs
+      v-if="isLoad"
+      v-model="activeName"
+      type="card"
+      class="demo-tabs"
+      @tab-click="handleClick"
+    >
+      <el-tab-pane label="Epoxy" name="epo"
+        ><Stock :stock="epoList"></Stock
+      ></el-tab-pane>
+      <el-tab-pane label="Package" name="package">
+        <Stock :stock="packList"></Stock>
+      </el-tab-pane>
+      <el-tab-pane label="Wire" name="wire"
+        ><Stock :stock="wireList"></Stock
+      ></el-tab-pane>
+      <el-tab-pane label="Capacitor" name="capacitor"
+        ><Stock :stock="capacitorList"></Stock
+      ></el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted, ref } from "vue";
+import { testData as t } from "./testingData";
+import axios from "axios";
+import Stock from "./StockList.vue";
+import type { TabsPaneContext } from "element-plus";
+import { getTodayDate, adjustDate } from "../../../../utils/date-utils";
+
+export interface MaterialStock {
+  MaterialId: string; // 재료 ID
+  Description: string; // 재료 설명
+  StockSap: number; // 재고 SAP
+  QtyInput: number; // 입력된 수량
+  Stock: number; // 재고 수량
+  OnHandDate: string; // 보유 날짜
+  DateCheck: string; // 확인 날짜
+  Unit: string; // 단위 (예: K, EA, SH)
+  DEFAULT_QTY: number; // 기본 수량
+  StockSH: number; // 재고 SH
+}
+
+const isLoad = ref<boolean>(false);
+const activeName = ref("epo");
+const wholeData = ref<MaterialStock[]>([]);
+const epoList = ref<MaterialStock[]>([]);
+const packList = ref<MaterialStock[]>([]);
+const wireList = ref<MaterialStock[]>([]);
+const capacitorList = ref<MaterialStock[]>([]);
+// onMounted(() => {
+//   wholeData.value = t.Data;
+
+//   for (let i = 0; i < wholeData.value.length; i++) {
+//     const material = wholeData.value[i];
+//     if (material.MaterialId.startsWith("010")) {
+//       epoList.value.push(material); // MaterialId가 "010"으로 시작하면 epoList에 추가
+//     } else {
+//       packList.value.push(material); // 그렇지 않으면 packList에 추가
+//     }
+//   }
+
+//   isLoad.value = true;
+// });
+
+const handleClick = (tab: TabsPaneContext, event: Event) => {};
+
+async function fetchData() {
+  // API URL
+  const url =
+    "/OpeationMns/ScheduleSample/CSPMaterialStock?date=" + getTodayDate();
+
+  // 헤더 설정
+  const headers = {
+    "sample-api-key":
+      "dc5b5fcf95907d906288117541b94ac8bbec1e1e5131593f28f4b1dcf7d8600c",
+  };
+
+  try {
+    // GET 요청
+    const response = await axios.get(url, { headers });
+
+    wholeData.value = response.data.Data;
+
+    for (let i = 0; i < wholeData.value.length; i++) {
+      const material = wholeData.value[i];
+      if (material.Description === "EPOXY"){
+        epoList.value.push(material)
+      }
+      else if(material.Description === "Wire-MKE-UB"){
+        wireList.value.push(material)
+      }
+      else if (material.MaterialId.startsWith("010")) {
+        if (material.MaterialId.startsWith("0102")) {
+          wireList.value.push(material);
+        } else {
+          epoList.value.push(material); // MaterialId가 "010"으로 시작하면 epoList에 추가
+        }
+      } else if (material.MaterialId.startsWith("2203")) {
+        capacitorList.value.push(material); // 그렇지 않으면 packList에 추가
+      } else {
+        packList.value.push(material); // 그렇지 않으면 packList에 추가
+      }
+    }
+
+    isLoad.value = true;
+  } catch (error) {
+    // 요청 실패
+    if (axios.isAxiosError(error)) {
+      console.error("요청 실패:", error.response?.status, error.response?.data);
+    } else {
+      console.error("알 수 없는 오류:", error);
+    }
+  }
+}
+
+onMounted(async () => {
+  await fetchData();
+});
+</script>
+
+<script lang="ts">
+export default { components: {} };
+</script>
+
+<style></style>
---
+++ src/views/Solder/Stock/Package/StockList.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <el-table :data="props.stock" style="width: 100%">
+      <el-table-column
+        label="Mat ID"
+        prop="MaterialId"
+        :align="'center'"
+      ></el-table-column>
+      <el-table-column
+        label="Desc"
+        prop="Description"
+        :align="'center'"
+      ></el-table-column>
+      <el-table-column
+        label="OnHand Date"
+        prop="OnHandDate"
+        :align="'center'"
+      ></el-table-column>
+      <el-table-column
+        label="OnHand(Stock SAP)"
+        prop="StockSap"
+        :align="'center'"
+      ></el-table-column>
+      <el-table-column
+        label="Qty Input"
+        prop="QtyInput"
+        :align="'center'"
+      ></el-table-column>
+      <el-table-column
+        label="Stock"
+        prop="Stock"
+        :align="'center'"
+      ></el-table-column>
+      <el-table-column
+        label="Unit"
+        prop="Unit"
+        :align="'center'"
+      ></el-table-column>
+      <el-table-column
+        label="Base Chip(EA/1SH)"
+        prop="DEFAULT_QTY"
+        :align="'center'"
+      ></el-table-column>
+      <el-table-column
+        label="Stock (SH)"
+        prop="StockSH"
+        :align="'center'"
+      ></el-table-column>
+      <el-table-column
+        label="Date"
+        prop="DateCheck"
+        :align="'center'"
+      ></el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { MaterialStock } from "./Package.vue";
+const props = defineProps<{ stock: MaterialStock[] }>();
+</script>
+
+<script lang="ts">
+export default { components: {} };
+</script>
+
+<style></style>
