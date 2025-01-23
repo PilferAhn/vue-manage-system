841f1da4016b7821d4b36fc0881f8422acea7f66
PilferAhn <49383308+PilferAhn@users.noreply.github.com>
2025-01-23 09:36:47 +0900

    header get method 추가.

--- src/utils/httpProtocol.ts
+++ src/utils/httpProtocol.ts
@@ -21,3 +21,38 @@ export const sendPostRequest = async (url: string, formData: FormData) => {
     throw error; // 에러를 다시 throw하여 호출자에게 에러를 알림
   }
 };
+
+
+
+export const sendGetRequestWithHeader = async (url : string , headers : object) => {
+  
+  try {
+    const response = await axios.get(url, { headers });
+    return response.data; // 응답 데이터를 반환
+  } catch (error) {
+    console.error("HTTP GET request failed:", error);
+    throw error; // 에러를 다시 throw하여 호출자에게 에러를 알림
+  }
+};
+
+export async function sendPostRequestWithHeader(url: string, formData: FormData){
+
+  try {
+    const response = await axios.post(url, formData, {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json, text/json, text/x-json, text/javascript, application/xml, text/xml",
+      },
+    });
+
+    
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error("Axios 에러:", error.response?.data || error.message);
+    } else {
+      console.error("알 수 없는 에러:", error);
+    }
+    return null;
+  }
+}
