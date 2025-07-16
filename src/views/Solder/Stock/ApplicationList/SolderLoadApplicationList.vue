ac0f6c05adacb6a0ab4029ef1bb09dd326726749
Shimko Aleksei <aleksey.shimko@gmail.com>
2025-07-16 13:10:02 +0900

    Refactor SolderLoadApplicationList.vue: clean up template and script formatting for improved readability

--- src/views/Solder/Stock/ApplicationList/SolderLoadApplicationList.vue
+++ src/views/Solder/Stock/ApplicationList/SolderLoadApplicationList.vue
@@ -1,21 +1,20 @@
 <!-- LoadApplicationList.vue -->
 <template>
-    <div>
-      <ApplicationListTemplate :stockItems="stockItems" :operationType="'sample'"/>
-    </div>
-  </template>
-  
-  <script setup lang="ts">
-  import { ref, onMounted } from "vue";
-  import type { StockItem } from "../../../../interface/stock";
-  import ApplicationListTemplate from "./ApplicationListTemplate.vue";
-  import { fetchStockItems } from "./ApplicationList"; // Import the fetch function
-  
-  const stockItems = ref<StockItem[]>([]);
-  
-  onMounted(async () => {
-    // Fetch StockInfo data on component mount
-    stockItems.value = await fetchStockItems("sample");
-  });
-  </script>
-  
+  <div>
+    <ApplicationListTemplate :stockItems="stockItems" :operationType="'sample'" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import type { StockItem } from "../../../../interface/stock";
+import ApplicationListTemplate from "./ApplicationListTemplate.vue";
+import { fetchStockItems } from "./ApplicationList"; // Import the fetch function
+
+const stockItems = ref<StockItem[]>([]);
+
+onMounted(async () => {
+  // Fetch StockInfo data on component mount
+  stockItems.value = await fetchStockItems("sample");
+});
+</script>
