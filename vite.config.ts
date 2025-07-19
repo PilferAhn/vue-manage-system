04a4ae56de932ef16db3132bb0ac337893a11294
Shimko Aleksei <aleksey.shimko@gmail.com>
2025-07-19 18:01:21 +0900

    Fix proxy configuration: change '/reel' endpoint to '/stock/' in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -66,7 +66,7 @@ export default defineConfig({
 				"/bfsm2" : "http://10.20.10.114",			
 				"/mes" : "http://10.29.11.57:40000",
 				"/OpeationMns" : "http://10.70.21.216:9086",
-				"/reel" : "http://10.29.11.57:40000",
+				"/stock/" : "http://10.29.11.57:40000",
 				"/dv2" : "http://10.29.11.57:40000",
 				"/test" : "http://10.29.11.59:8002",
 				"/fab_monitoring_rev2": "http://10.29.11.57:40000",
